f18fab51f4c6bd619270a66d5531b7c783c156c0
Andrey Sazonov <61043163+firefighter-19@users.noreply.github.com>
2020-06-02 20:37:40 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -37,14 +37,14 @@ changeBg ();
 
 let changedHeight = $('')
 
-// jQuery(document).ready(function() {
-//   jQuery(".item__link").click(function () {
-//   elementClick = jQuery(this).attr("href")
-//   destination = jQuery(elementClick).offset().top - 77;
-//   jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 700);
-//   return false;
-//   });
-// });
+jQuery(document).ready(function() {
+  jQuery(".item__link").click(function () {
+  elementClick = jQuery(this).attr("href")
+  destination = jQuery(elementClick).offset().top - 77;
+  jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 700);
+  return false;
+  });
+});
 
 const burgerSlide = () => {
   const burger = document.querySelector('.nav__container__burger');
@@ -84,26 +84,153 @@ $('.nav__menu__list-item li').removeAttr( 'style' );
 
 //===========================MODAL__WINDOW=============================
 
-const body = document.querySelector('body')
-const modal = document.querySelector('.modal')
-const openModalButton = document.querySelector('.open__modal')
-const closeModalButton = document.querySelector('.modal__close')
+// const body = document.querySelector('body')
+// const modal = document.querySelector('.modal')
+// const openModalButton = document.querySelector('.open__modal')
+// const closeModalButton = document.querySelector('.modal__close')
+//
+// function existVerticalScroll() {
+//    return document.body.offsetHeight > window.innerHeight
+// }
+//
+// function getBodyScrollTop() {
+//     return self.pageYOffset || (document.documentElement && document.documentElement.ScrollTop) || (document.body && document.body.scrollTop);
+// }
+//
+//
+// openModalButton.addEventListener('click',() => {
+//     body.classList.toggle('lock');
+//     modal.classList.add('modal__opened');
+// })
+//
+// closeModalButton.addEventListener('click',() => {
+//   body.classList.remove('lock');
+//   modal.classList.remove('modal__opened');
+// });
 
-function existVerticalScroll() {
-   return document.body.offsetHeight > window.innerHeight
+
+//===============================POPUP__WINDOW==================================
+
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding');
+
+let unlock = true;
+
+const timeout = 300;
+
+if (popupLinks.length > 0) {
+  for (let index = 0; index < popupLinks.length; index++) {
+    const popupLink = popupLinks[index];
+    popupLink.addEventListener('click', function (e) {
+      const popupName = popupLink.getAttribute('href').replace('#', '');
+      const currentPopup = document.getElementById(popupName);
+      popupOpen(currentPopup);
+      e.preventDefault();
+    });
+  }
 }
 
-function getBodyScrollTop() {
-    return self.pageYOffset || (document.documentElement && document.documentElement.ScrollTop) || (document.body && document.body.scrollTop);
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+if (popupCloseIcon.length > 0) {
+  for (let index = 0; index < popupCloseIcon.length; index++) {
+    const el = popupCloseIcon[index];
+    el.addEventListener('click', function (e) {
+      popupClose(el.closest('.popup'));
+      e.preventDefault();
+    });
+  }
 }
 
+function popupOpen (currentPopup) {
+  if (currentPopup && unlock) {
+    const popupActive = document.querySelector('.popup.open');
+    if (popupActive) {
+      popupClose(popupActive, false);
+    } else {
+      bodyLock();
+    }
+    currentPopup.classList.add('open');
+    currentPopup.addEventListener('click', function (e) {
+      if (!e.target.closest('.popup__content')) {
+        popupClose(e.target.closest('.popup'));
+      }
+    });
+  }
+}
 
-openModalButton.addEventListener('click',() => {
-    body.classList.toggle('lock');
-    modal.classList.add('modal__opened');
-})
+function popupClose(popupActive, doUnlock = true) {
+  if (unlock) {
+    popupActive.classList.remove('open');
+    if (doUnlock) {
+        bodyUnLock();
+    }
+  }
+}
 
-closeModalButton.addEventListener('click',() => {
-  body.classList.remove('lock');
-  modal.classList.remove('modal__opened');
+function bodyLock() {
+  const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+
+  if (lockPadding.length > 0) {
+    for (let index = 0; index < lockPadding.length; index++) {
+      const el = lockPadding[index];
+      el.style.paddingRight = lockPaddingValue;
+    }
+  }
+  body.style.paddingRight = lockPaddingValue;
+  body.classList.add('lock');
+
+  unlock = false;
+  setTimeout(function () {
+    unlock = true;
+  }, timeout);
+}
+
+function bodyUnLock() {
+  setTimeout(function () {
+    if (lockPadding.length > 0) {
+      for (let index = 0; index < lockPadding.length; index++) {
+        const el = lockPadding[index];
+        el.style.paddingRight = '0px';
+      }
+    }
+    body.style.paddingRight = '0px';
+    body.classList.remove('lock');
+  }, timeout);
+
+  unlock = false;
+  setTimeout(function () {
+    unlock = true;
+  }, timeout);
+}
+
+document.addEventListener('keydown', function (e) {
+  if (e.which === 27) {
+    const popupActive = document.querySelector('.popup.open');
+    popupClose(popupActive);
+  }
 });
+
+
+(function () {
+  if (!Element.prototype.closest) {
+
+    Element.prototype.closest = function (css) {
+      var node = this;
+      while (node) {
+        if (node.matches(css)) return node;
+        else node = node.parentElement;
+      }
+      return null;
+    };
+  }
+})();
+
+(function (){
+  if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.matchesSelector ||
+      Element.prototype.webkitMatchesSelector ||
+      Element.prototype.mozMatchesSelector ||
+      Element.prototype.msMatchesSelector;
+  }
+})();
